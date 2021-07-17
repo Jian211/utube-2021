9d33fbab0f3f6aa73d46af3bf60fc05003ce709a
jian-level-is <myunggeun22@gmail.com>
2021-07-17 19:47:58 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,13 @@
 import express from "express"
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
 
-const handle = (req, res, next) => {
-    console.log(req.url, "hi");
-    next();
-}
 
-//middleware
-app.use(handle);
+app.use(morgan("dev"));
 
 app.get("/", ()=> console.log("Someone came to my homepage wow!!"));
 
 
-
-app.listen(PORT,() => console.log(`Server lisenting on http://localhost:${PORT}`));
+app.listen(PORT,() => console.log(`Server lisenting`));
