b1757c3c7269903cd01172be6df644f29ea49d3f
jian-level-is <myunggeun22@gmail.com>
2021-07-18 17:28:13 +0900

    Create userControllers.js

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,6 @@
+export const join   = (req,res) => res.send("welcome Join!");
+export const login  = (req,res) => res.send("login kudasai");
+
+
+export const edit   = (req,res) => res.send("Edit user");
+export const remove = (req,res) => res.send("remove user");
