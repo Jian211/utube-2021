72e0f5a201c1c16502dd116cadb475ebcc8cb620
jian-level-is <myunggeun22@gmail.com>
2021-07-18 17:28:16 +0900

    Create videoControllers.js

---
+++ src/controllers/videoControllers.js
@@ -0,0 +1,4 @@
+export const home   = (req,res) => res.send("Home Video");
+
+export const editVideo   = (req,res) => res.send("Edit Video");
+export const removeVideo = (req,res) => res.send("remove Video");
