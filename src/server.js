84975d6cdddd991eb971a1128d79751f086c1ee9
jian-level-is <myunggeun22@gmail.com>
2021-07-18 17:28:26 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import express from "express"
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 const app = express();
 
+// Routers
+app.use("/", globalRouter);
+app.use("/user",userRouter);
+app.use("/video",videoRouter);
 
+// Middleware
 app.use(morgan("dev"));
-
-app.get("/", ()=> console.log("Someone came to my homepage wow!!"));
 
 
 app.listen(PORT,() => console.log(`Server lisenting`));
