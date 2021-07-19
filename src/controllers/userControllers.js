67366fd4f2b72a17bd07c45ef947761cb0e188cc
jian-level-is <myunggeun22@gmail.com>
2021-07-19 18:32:54 +0900

    Update userControllers.js

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,12 @@
+// to globalRouter.
 export const join   = (req,res) => res.send("welcome Join!");
 export const login  = (req,res) => res.send("login kudasai");
 
-
+// to userRouter.
 export const edit   = (req,res) => res.send("Edit user");
 export const remove = (req,res) => res.send("remove user");
+export const watch  = (req,res) => {
+    console.log(req.params.id);
+    return res.send(`Watch User #${req.params.id}`);
+}
+export const logout = (req,res) => res.send("Log Out");
