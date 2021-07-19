5eb3741e039a84e160323405edeafbae0c71ac7e
jian-level-is <myunggeun22@gmail.com>
2021-07-19 18:33:06 +0900

    Update userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { edit, remove } from "../controllers/userControllers"
+import { edit, remove, watch, logout} from "../controllers/userControllers"
 
 const userRouter   = express.Router();
 
 userRouter.get("/edit",edit);
 userRouter.get("/remove", remove);
+userRouter.get("/logout", logout);
+userRouter.get("/:id(\\d+)", watch);
 
 export default userRouter
