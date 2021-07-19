2113cd6c979442a6f69b3854744db733038ee856
jian-level-is <myunggeun22@gmail.com>
2021-07-19 18:33:00 +0900

    Update videoControllers.js

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,14 @@
 export const home   = (req,res) => res.send("Home Video");
+export const search = (req,res) => res.send("Search Video");
 
-export const editVideo   = (req,res) => res.send("Edit Video");
-export const removeVideo = (req,res) => res.send("remove Video");
+//Video router
+export const edit   = (req,res) => {
+    
+    return res.send(`Edit Video #${req.params.id}`);
+}
+    
+export const remove = (req,res) => res.send("remove Video");
+export const upload = (req,res) => res.send("Upload Video");
+export const watch  = (req,res) => {
+     return res.send(`watch Video #${req.params.id}`);
+}
