0b37f019ef279a29d2ff3f6d0866b5c0ff85c870
jian-level-is <myunggeun22@gmail.com>
2021-07-21 18:39:35 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,16 +1,40 @@
 import express from "express"
 
+const PORT = 4000;
 const app = express();
 
-const handleHome    = (req, res) => { return res.send("HOME PAGE")};
-const handleAbout   = (req, res) => { return res.send("ABOUT")};
-const handleContact = (req, res) => { return res.send("Contact")};
-const handleLogin   = (req, res) => { return res.send("Login")};
+const home    = (req, res) => { return res.send("HOME PAGE")};
+const protector   = (req, res ,next) => {
+    if(req.url === "/protected"){
+        return res.end();
+    }
+    next();
+};
 
+const urlLogger = (req,res,next) => {
+    console.log("path: ",req.url);
+    next();    
+};
 
-app.get("/", handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("login",handleLogin);
+const timeLogger = (req,res,next) =>{
+    const date  = new Date();
+    const year  = date.getFullYear();
+    const month = date.getMonth();
+    const day   = date.getDate();
+    
+    console.log("time:",`${year}.${month}.${day}`);
+    next();
+}
+const secure = (req,res,next) => {
+    if(req.protocol ===`https`){
+        console.log("secure");
+    }else{
+        console.log("insecure");
+    }
+    next();
+}
+
+app.get("/", urlLogger,timeLogger,secure,home);
+app.get("/protected", protector);
 
 app.listen(PORT,() => console.log(`Server lisenting`));
