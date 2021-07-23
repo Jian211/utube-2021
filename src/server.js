e8b2a7a65bbac1b4b73f4abe6d99b259816babec
jian-level-is <myunggeun22@gmail.com>
2021-07-23 12:57:03 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,40 +1,12 @@
 import express from "express"
+//route
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import storyRouter from "./routers/storyRouter";
 
 const PORT = 4000;
 const app = express();
 
-const home    = (req, res) => { return res.send("HOME PAGE")};
-const protector   = (req, res ,next) => {
-    if(req.url === "/protected"){
-        return res.end();
-    }
-    next();
-};
-
-const urlLogger = (req,res,next) => {
-    console.log("path: ",req.url);
-    next();    
-};
-
-const timeLogger = (req,res,next) =>{
-    const date  = new Date();
-    const year  = date.getFullYear();
-    const month = date.getMonth();
-    const day   = date.getDate();
-    
-    console.log("time:",`${year}.${month}.${day}`);
-    next();
-}
-const secure = (req,res,next) => {
-    if(req.protocol ===`https`){
-        console.log("secure");
-    }else{
-        console.log("insecure");
-    }
-    next();
-}
-
-app.get("/", urlLogger,timeLogger,secure,home);
-app.get("/protected", protector);
+app.use(globalRouter,userRouter,storyRouter);
 
 app.listen(PORT,() => console.log(`Server lisenting`));
