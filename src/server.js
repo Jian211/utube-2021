e00ed82fe6f1cde9226b5ec2c998d0f272322cae
jian-level-is <myunggeun22@gmail.com>
2021-07-26 23:13:33 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ import storyRouter from "./routers/storyRouter";
 const PORT = 4000;
 const app = express();
 
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + `/src/views`);
+
 app.use(globalRouter,userRouter,storyRouter);
 
 app.listen(PORT,() => console.log(`Server lisenting`));
